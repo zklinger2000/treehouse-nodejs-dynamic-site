584e36dad606e207981fa23461a6528cc7eb6006
Zack <zklinger@gmail.com>
2015-05-19 19:25:06 -0700

    CREATE function homeRoute(request, response) to test our server

--- app.js
+++ app.js
@@ -7,20 +7,21 @@
 var http = require('http');
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  setInterval(function() {
-	  response.write(new Date() + '\n');
-  }, 1000);
-  //response.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:1337/');
+	homeRoute(request, response);
+}).listen(1337);
+console.log("Server started at 127.0.0.1:1337");
 
 //Handle HTTP route GET / and POST / i.e. Home
+function homeRoute(request, response) {
 	//if url == "/" && GET
 		//show search
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.write("Header\n");
+		response.write("Search\n");
+		response.end("Footer\n");
 	//if url == "/" && POST
 		//redirect to /:username
+}
 
 //Handle HTTP route GET /:username i.e. /chalkers
 	//if url -- "/...."
