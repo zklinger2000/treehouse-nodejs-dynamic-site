1c91cf04f23ffdf0d09c8457f810073b2129d62a
Zack <zklinger@gmail.com>
2015-05-20 16:02:25 -0700

    Final version from course

--- router.js
+++ router.js
@@ -1,20 +1,33 @@
 var Profile = require('./profile.js');
 var renderer = require('./renderer.js');
+var querystring = require('querystring');
+var commonHeaders = {'Content-Type': 'text/html'};
 
 //Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
 	//if url == "/" && GET
 	if (request.url === "/")
 	{
-		//show search
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		renderer.view('header', {}, response);
-		renderer.view('search', {}, response);
-		renderer.view('footer', {}, response);
-		response.end();
+		if (request.method.toLowerCase() === "get") {
+			//show search
+			response.writeHead(200, commonHeaders);
+			renderer.view('header', {}, response);
+			renderer.view('search', {}, response);
+			renderer.view('footer', {}, response);
+			response.end();
+		} else {
+			//if url == "/" && POST
+
+			//get the post data from body
+			request.on('data', function(postBody) {
+				//extract the username
+				var query = querystring.parse(postBody.toString());
+				response.writeHead(303, {'Location': '/' + query.username});
+				response.end();
+				//redirect to /:username
+			});
+		}
 	}
-	//if url == "/" && POST
-		//redirect to /:username
 }
 
 //Handle HTTP route GET /:username i.e. /chalkers
@@ -24,7 +37,7 @@ function user(request, response)
 	var username = request.url.replace("/", "");
 	if (username.length > 0)
 	{
-		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.writeHead(200, commonHeaders);
 		renderer.view('header', {}, response);
 		//get json from Treehouse
 		var studentProfile = new Profile(username);
